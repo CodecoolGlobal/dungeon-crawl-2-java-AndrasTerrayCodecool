7c9b202cdb9aa67342c5bf6a8f26c9290d653ecd
Andras Terray <andras.terray@codecool.com>
2021-09-15 08:12:48 +0200

    Add base items to pick up

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Key.java
@@ -0,0 +1,26 @@
+package com.codecool.dungeoncrawl.logic.actors;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class Key {
+    private Cell cell;
+    private int colorRed;
+    private int colorGreen;
+    private int colorBlue;
+
+    public Key(Cell cell, int colorRed, int colorGreen, int colorBlue) {
+        this.cell = cell;
+        this.colorRed = colorRed;
+        this.colorGreen = colorGreen;
+        this.colorBlue = colorBlue;
+    }
+
+    public void pickUp(Actor actor) {
+        actor.getItem(actor);
+        if(actor.getHealth() > 100) {
+            colorRed = 0;
+            colorGreen = 0;
+            colorBlue = 0;
+        }
+    }
+}
